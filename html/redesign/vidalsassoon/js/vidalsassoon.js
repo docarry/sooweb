5a649129bd14ca50575c26918d538fe852ac4210
soojihan <hdocarry@gmail.com>
2025-03-08 00:55:30 +0900

    docs: header update

--- html/redesign/vidalsassoon/js/vidalsassoon.js
+++ html/redesign/vidalsassoon/js/vidalsassoon.js
@@ -3,16 +3,16 @@ new fullpage('#fullpage', {
     scrollHorizontally: true,
     anchors: ['banner', 'brandStory', 'bestSeller', 'category', 'media', 'news', 'footer'],
     menu: '#menu',
-    afterLoad: function(origin, destination, direction) {
-        const anchorLink = destination.anchor; // 현재 섹션의 anchor
+    // afterLoad: function(origin, destination, direction) {
+    //     const anchorLink = destination.anchor; // 현재 섹션의 anchor
     
-        // 'banner' 섹션을 제외한 다른 섹션들에서만 scrolled 클래스 추가
-        if (anchorLink !== 'banner') {
-            $('#header').addClass('scrolled');
-        } else {
-            $('#header').removeClass('scrolled');
-        }
-    }
+    //     // 'banner' 섹션을 제외한 다른 섹션들에서만 scrolled 클래스 추가
+    //     if (anchorLink !== 'banner') {
+    //         $('#header').addClass('scrolled');
+    //     } else {
+    //         $('#header').removeClass('scrolled');
+    //     }
+    // }
 });
 
 // 03 베스트셀러
